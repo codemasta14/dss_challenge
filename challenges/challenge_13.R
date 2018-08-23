20f5b4916596b0027e690cb4ebe273beea81e5f9
Cody <codemasta14@gmail.com>
2018-08-22 23:54:45 -0600

    slight change to 13

--- challenges/challenge_13.R
+++ challenges/challenge_13.R
@@ -7,6 +7,7 @@ cody <- "Q29uZ3JhdHVsYXRpb25zISBZb3UgaGF2ZSBzb2x2ZWQgQ2hhbGxlbmdlIDEzLiBZb3UgYXJ
 
 s <- base64enc::base64decode(cody)%>%
   str_flatten()
+#Janky code I found online that converts hexadecimal into ascii
 h <- sapply(seq(1, nchar(s), by=2), function(x) substr(s, x, x+1))
 rawToChar(as.raw(strtoi(h, 16L)))
 
